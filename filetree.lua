f5a89d837d2d266fe5860b3422b8b703a351c8c1
Nicolai Søborg <NicolaiSoeborg@users.noreply.github.com>
2016-12-11 01:43:58 +0100

    Fix NewBuffer + no save prompt for tree view

--- filetree.lua
+++ filetree.lua
@@ -4,7 +4,7 @@ treeView = nil
 
 function OpenTree()
     local origNum = CurView().Num
-    CurView():VSplitIndex(NewBuffer(nil, "Filetree"), 0)
+    CurView():VSplitIndex(NewBuffer("Filetree", ""), 0)
     CurView().Width = 30
     CurView().LockWidth = true
     tabs[curTab+1]:Resize()
@@ -12,6 +12,8 @@ function OpenTree()
     treeView = CurView()
 
     treeView.Buf:Insert(Loc(0, 0), table.concat(scandir("."), "\n"))
+    treeView.Buf.Settings["autosave"] = false
+    treeView.Buf.IsModified = false
 
     tabs[curTab+1].CurView = origNum + 1
 end
