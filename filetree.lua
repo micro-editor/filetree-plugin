24d43e47126f91ab80d719f7785d31e09a7376d7
Nicolai Søborg <NicolaiSoeborg@users.noreply.github.com>
2016-12-11 17:38:08 +0100

    Simple filebrowser implemented
    It is still very alpha, but a simple file browser has been implemented

--- filetree.lua
+++ filetree.lua
@@ -1,23 +1,75 @@
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 treeView = nil
+cwd = "."
 
 function OpenTree()
     local origNum = CurView().Num
-    CurView():VSplitIndex(NewBuffer("Filetree", ""), 0)
+    CurView():VSplitIndex(NewBuffer("", ""), 0)
     CurView().Width = 30
     CurView().LockWidth = true
     tabs[curTab+1]:Resize()
 
     treeView = CurView()
-
-    treeView.Buf:Insert(Loc(0, 0), table.concat(scandir("."), "\n"))
-    treeView.Buf.Settings["autosave"] = false
-    treeView.Buf.IsModified = false
-
-    tabs[curTab+1].CurView = origNum + 1
+    RefreshTree()
+    LoseFocus(origNum)
 end
 
+function LoseFocus(num)
+    tabs[curTab+1].CurView = num + 1
+end
+
+function RefreshTree()
+    --if treeView == nil then OpenTree() end
+    treeView.Buf:remove(treeView.Buf:Start(), treeView.Buf:End())
+    treeView.Buf:Insert(Loc(0,0), table.concat(scandir(cwd), "\n"))
+    treeView.Buf.Settings["softwrap"] = false
+    treeView.Buf.Settings["autosave"] = false
+    treeView.Buf.IsModified = false
+end
+
+-- When user press enter
+function preInsertNewline(view)
+    if view == treeView then
+        local selected = view.Buf:Line(view.Cursor.Loc.Y)
+        if isDir(selected) then
+            cwd = cwd .. "/" .. selected
+            RefreshTree()
+        else
+            -- TODO: NewBuffer calls NewBufferFromString
+            -- ... so manually read file content:
+            local filename = cwd .. "/" .. selected
+            local filehandle = io.open(filename, "r")
+            if not filehandle then
+                TermMessage("Can't open file:", filename)
+            end
+            local filecontent = filehandle:read("*all")
+            CurView():VSplitIndex(NewBuffer(filecontent, filename), 0)
+            tabs[curTab+1]:Resize()
+            LoseFocus(CurView().Num)
+        end
+        return false
+    end
+    return true
+end
+
+--[[ allows for deleting files
+function preDelete(view)
+    if view == treeView then
+        messenger:YesNoPrompt("Do you want to delete ...?")
+        return false
+    end
+    return true
+end
+]]--
+
+-- don't prompt to save tree view
+function preQuit(view)
+    if view == treeView then
+        view.Buf.IsModified = false
+    end
+    return true
+end
 
 function scandir(directory)
     local i, t, popen = 0, {}, io.popen
@@ -28,6 +80,16 @@ function scandir(directory)
     end
     pfile:close()
     return t
+end
+
+function isDir(path)
+    local pfile = io.popen('ls -adl "' .. cwd .. "/" .. path .. '"')
+    local status = false
+    if pfile:read(1) == "d" then
+        status = true
+    end
+    pfile:close()
+    return status
 end
 
 MakeCommand("tree", "filetree.OpenTree", 0)
