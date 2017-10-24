580c2c24228e94a55a450b866cd2f12f677160dc
Nicolai Søborg <git@xn--sb-lka.org>
2017-10-24 23:41:25 +0200

    Change debugging

--- filemanager.lua
+++ filemanager.lua
@@ -4,11 +4,15 @@ treeView = nil
 cwd = WorkingDirectory()
 driveLetter = "C:\\"
 isWin = (OS == "windows")
-debug = false
+
+-- Uncomment to enable debugging
+function debug(log)
+    -- messenger:AddLog(log)
+end
 
 -- ToggleTree will toggle the tree view visible (create) and hide (delete).
 function ToggleTree()
-    if debug == true then messenger:AddLog("***** ToggleTree() *****") end
+    debug("***** ToggleTree() *****")
     if treeView == nil then
         OpenTree()
     else
@@ -18,7 +22,7 @@ end
 
 -- OpenTree setup's the view
 function OpenTree()
-    if debug == true then messenger:AddLog("***** OpenTree() *****") end
+    debug("***** OpenTree() *****")
     CurView():VSplitIndex(NewBuffer("", "FileManager"), 0)
     setupOptions()
     refreshTree()
@@ -26,7 +30,7 @@ end
 
 -- setupOptions setup tree view options
 function setupOptions()
-    if debug == true then messenger:AddLog("***** setupOptions() *****") end
+    debug("***** setupOptions() *****")
     treeView = CurView()
     treeView.Width = 30
     treeView.LockWidth = true
@@ -45,7 +49,7 @@ end
 
 -- CloseTree will close the tree plugin view and release memory.
 function CloseTree()
-    if debug == true then messenger:AddLog("***** CloseTree() *****") end
+    debug("***** CloseTree() *****")
     if treeView ~= nil then
         treeView.Buf.IsModified = false
         treeView:Quit(false)
@@ -55,24 +59,22 @@ end
 
 -- refreshTree will remove the buffer and load contents from folder
 function refreshTree()
-    if debug == true then messenger:AddLog("***** refreshTree() *****") end
-    -- if debug == true then messenger:AddLog("Start -> ",treeView.Buf:Start()," End -> ",treeView.Buf:End()) end
+    debug("***** refreshTree() *****")
     treeView.Buf:remove(treeView.Buf:Start(), treeView.Buf:End())
     local list = table.concat(scanDir(cwd), "\n ")
-    if debug == true then messenger:AddLog("dir -> ",list) end
     treeView.Buf:Insert(Loc(0,0),list)
 end
 
 -- returns currently selected line in treeView
 function getSelection()
-    if debug == true then messenger:AddLog("***** getSelection() ---> ",treeView.Buf:Line(treeView.Cursor.Loc.Y):sub(2)) end
+    debug("***** getSelection() ---> ",treeView.Buf:Line(treeView.Cursor.Loc.Y):sub(2))
     return (treeView.Buf:Line(treeView.Cursor.Loc.Y)):sub(2)
 end
 
 -- don't use built-in view.Cursor:SelectLine() as it will copy to clipboard (in old versions of Micro)
 function selectLineInTree(view)
     if view == treeView then
-        if debug == true then messenger:AddLog("***** selectLineInTree(view) *****") end
+        debug("***** selectLineInTree() *****")
         local y = view.Cursor.Loc.Y
         view.Cursor.CurSelection[1] = Loc(0, y)
         view.Cursor.CurSelection[2] = Loc(view.Width, y)
@@ -87,7 +89,7 @@ function onCursorUp(view)   selectLineInTree(view) end
 function onMousePress(view, event)
     if view == treeView then  -- check view is tree as only want inputs from that view.
          local columns, rows = event:Position()
-         if debug == true then messenger:AddLog("INFO: --> Mouse pressed -> columns location rows location -> ",columns,rows) end
+         debug("INFO: --> Mouse pressed -> columns location rows location -> ",columns,rows)
          return false
     end
 end
@@ -95,7 +97,7 @@ end
 -- disallow selecting topmost line in treeView:
 function preCursorUp(view)  
     if view == treeView then
-        if debug == true then messenger:AddLog("***** preCursor(view) *****") end
+        debug("***** preCursor(view) *****")
         if view.Cursor.Loc.Y == 1 then
             return false
 end end end
@@ -133,14 +135,12 @@ end
 -- If it is a file then open it in a new vertical view
 function preInsertNewline(view)
     if view == treeView then
-        if debug == true then messenger:AddLog("***** preInsertNewLine(view)  *****") end
+        debug("***** preInsertNewLine()  *****")
         local selected = getSelection()
         if view.Cursor.Loc.Y == 0 then
             return false -- topmost line is cwd, so disallowing selecting it
         elseif isDir(selected) then  -- if directory then reload contents of tree view
-            if debug == true then messenger:AddLog("current working directory -> ",cwd) end
             cwd = JoinPaths(cwd, selected)
-            if debug == true then messenger:AddLog("current working directory with selected directory -> ",cwd) end
             refreshTree()
         else  -- open file in new vertical view
             local filename = JoinPaths(cwd, selected)
@@ -157,7 +157,7 @@ end
 -- don't prompt to save tree view
 function preQuit(view)
     if view == treeView then
-        if debug == true then messenger:AddLog("***** preQuit(view) *****") end
+        debug("***** preQuit() *****")
         view.Buf.IsModified = false
     end
 end
@@ -165,7 +165,7 @@ function preQuitAll(view) treeView.Buf.IsModified = false end
 
 -- scanDir will scan contents of the directory passed.
 function scanDir(directory)
-    if debug == true then messenger:AddLog("***** scanDir(directory) ---> ",directory) end
+    debug("***** scanDir(directory) ---> ",directory)
     local i, list, proc = 3, {}, nil
     list[1] = (isWin and driveLetter or "") .. cwd  -- current directory working.
     list[2] = ".."  -- used for going up a level in directory.
@@ -186,7 +186,7 @@ end
 -- isDir checks if the path passed is a directory.
 -- return true if it is a directory else false if it is not a directory.
 function isDir(path)
-    if debug == true then messenger:AddLog("***** isDir(path) ---> ",path) end
+    debug("***** isDir(path) ---> ",path)
     local dir, proc = false, nil
     if isWin then
         proc = io.popen('IF EXIST ' .. driveLetter .. JoinPaths(cwd, path) .. '/* (ECHO d) ELSE (ECHO -)')
@@ -197,7 +197,7 @@ function isDir(path)
         dir = true
     end
     proc:close()
-    if debug == true then messenger:AddLog("is Dir Return = ",dir) end
+    debug("***** isDir return ",dir)
     return dir
 end
 
